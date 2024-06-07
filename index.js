167da3df6e0db9eb4cbb31b55c7285f2c50ae8a8
dpatel-bgworkforce <dpatel@bgworkforce.com>
2024-06-07 21:00:09 +0530

    Fix Cors Error

--- index.js
+++ index.js
@@ -9,7 +9,30 @@ import { userRoutes } from "./routes/user.routes.js";
 const app = express();
 app.disable("x-powered-by");
 app.use(express.json());
-app.use(cors({ origin: process.env.CORS_URL, credentials: true }));
+// app.use(
+//   cors({
+//     origin: "*",
+//     credentials: true,
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     allowedHeaders: ["Content-Type"],
+//   })
+// );
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  }),
+  function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  }
+);
 
 app.use(authorization, userRoutes);
 
